b0ef4e94471724dea26027f0d331d33df786c5f6
tinamznk <tinatuneplay@gmail.com>
2025-08-06 23:43:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ if (fs.existsSync(LOCK_FILE)) {
 
 const KEYWORDS = [
   { "keyword": "car bumper stickers and decals", "asin": "B0C99SLC1J" },
-  { "keyword": "bigfoot vinyl decalraccoon sticker", "asin": "B0C99SLC1J" },
-  { "keyword": "bumper sticker car", "asin": "B0C99SLC1J" },
+  { "keyword": "bigfoot vinyl decal", "asin": "B0C99SLC1J" },
+  { "keyword": "xxxxx", "asin": "B0C99SLC1J" },
   { "keyword": "best bumper stickers", "asin": "B0C99SLC1J" },
   { "keyword": "car stickers funny", "asin": "B0C99SLC1J" },
   { "keyword": "raccoon sticker", "asin": "B0C99SLC1J" }
